63e83e36981f23de6f96cadfafcb34fc931b594f
boreddad <ronnebaumfinances@gmail.com>
2024-03-03 10:33:10 -0500

    feat: hello world

--- cmd/weather/main.go
+++ cmd/weather/main.go
@@ -1,7 +1,18 @@
 package weather
 
-import "log"
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/vincer2040/weather/internal/render"
+	"github.com/vincer2040/weather/internal/routes"
+)
 
 func Main() {
-	log.Println("hi mom")
+	e := echo.New()
+
+	e.Renderer = render.New()
+	e.Use(middleware.Logger())
+	e.GET("/", routes.RootGet)
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
---
+++ internal/render/render.go
@@ -0,0 +1,23 @@
+package render
+
+import (
+	"html/template"
+	"io"
+
+	"github.com/labstack/echo/v4"
+)
+
+type Template struct {
+	templates *template.Template
+}
+
+func New() *Template {
+	t := &Template{
+		templates: template.Must(template.ParseGlob("public/*.html")),
+	}
+	return t
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
