b25793c332a832caeb683090e93a979475c9307e
vincer2040 <ronnebaumfinances@gmail.com>
2024-03-05 19:57:19 -0500

    feat: basic sign up post

--- internal/routes/signup.go
+++ internal/routes/signup.go
@@ -1,9 +1,11 @@
 package routes
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/vincer2040/weather/internal/types"
 	"github.com/vincer2040/weather/internal/wctx"
 	"github.com/vincer2040/weather/internal/wsession"
 )
@@ -23,4 +25,36 @@ func SignUpGet(c echo.Context) error {
 		"SessionData": sessionData,
 		"Route":       "signup",
 	})
+}
+
+func SignupPost(c echo.Context) error {
+	cc := c.(wctx.WCtx)
+	session, err := cc.Store.Get(cc.Request(), "session")
+	if err != nil {
+		return err
+	}
+	user := types.UserFromRequest(cc)
+	repeatedPassword := cc.FormValue("repeat-password")
+	if !user.IsValid() {
+		return errors.New("invalid user")
+	}
+	if user.Password != repeatedPassword {
+		return errors.New("passwords do not match")
+	}
+	err = user.HashPassword()
+	if err != nil {
+		return err
+	}
+	id, err := cc.DB.InsertUser(user)
+	if err != nil {
+		return err
+	}
+	session.Values["userID"] = id
+	session.Values["authenticated"] = true
+	err = session.Save(cc.Request(), cc.Response())
+	if err != nil {
+		return err
+	}
+	cc.Response().Header().Add("HX-Location", "/home")
+	return nil
 }
