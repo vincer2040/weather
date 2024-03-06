c24a87673d17c022a2884e8edf3547e89cf40ade
vincer2040 <ronnebaumfinances@gmail.com>
2024-03-05 20:47:48 -0500

    feat: basic me page

---
+++ internal/routes/me.go
@@ -0,0 +1,31 @@
+package routes
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/vincer2040/weather/internal/wctx"
+	"github.com/vincer2040/weather/internal/wsession"
+)
+
+func MeGet(c echo.Context) error {
+	cc := c.(wctx.WCtx)
+	session, err := cc.Store.Get(cc.Request(), "session")
+	if err != nil {
+		return err
+	}
+	err = wsession.Setup(session, cc.Request(), cc.Response())
+	if err != nil {
+		return err
+	}
+	sessionData := wsession.GetSessionData(session)
+	user, err := cc.DB.GetUserById(sessionData.UserId)
+	if err != nil {
+		return err
+	}
+	return cc.Render(http.StatusOK, "me.html", map[string]interface{}{
+		"SessionData": sessionData,
+		"Route":       "me",
+		"User":        user,
+	})
+}
